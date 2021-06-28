98cd215d85035c78b8ced352481847fc2c038c16
Lam-Pham <lpham127@gmail.com>
2021-06-27 22:14:38 -0700

    extended fetchJson to retrieve all page results

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,9 @@
 export async function fetchJson<Response = any>(url: string, init?: RequestInit): Promise<Response> {
-  const response = await fetch(
+  
+  let allResponses: any = []
+  let nextPage: string = ''
+
+  let response = await fetch(
     `https://swapi.dev/api/${url}/`,
     {
       ...init ?? {},
@@ -8,7 +12,25 @@ export async function fetchJson<Response = any>(url: string, init?: RequestInit)
         'Content-Type': 'application/json',
       }
     })
+      .then(res => res.json())
 
+  nextPage = await response.next
+  allResponses.push(response)
 
-  return response.json()
+  while(nextPage){
+    response = await fetch(
+      nextPage,
+      {
+        ...init ?? {},
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+        .then(res => res.json())
+    nextPage = await response.next
+    allResponses.push(response)
+  }
+
+  return allResponses
 }
